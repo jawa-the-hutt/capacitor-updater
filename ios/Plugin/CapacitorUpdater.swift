97a4d564e34de68d4d77ffa83be244a2c273ab8d
Jawa-the-Hutt <mcsegary@yahoo.com>
2024-03-06 11:22:00 -0600

    finish up ios

--- ios/Plugin/CapacitorUpdater.swift
+++ ios/Plugin/CapacitorUpdater.swift
@@ -168,6 +168,7 @@ enum CustomError: Error {
     case cannotUnflat
     case cannotCreateDirectory
     case cannotDeleteDirectory
+    case cannotDecryptSessionKey
 
     // Throw in all other cases
     case unexpected(code: Int)
@@ -204,12 +205,17 @@ extension CustomError: LocalizedError {
         case .cannotDecode:
             return NSLocalizedString(
                 "Decoding the zip failed with this key",
-                comment: "Invalid private key"
+                comment: "Invalid public key"
             )
         case .cannotWrite:
             return NSLocalizedString(
                 "Cannot write to the destination",
                 comment: "Invalid destination"
+            )
+        case .cannotDecryptSessionKey:
+            return NSLocalizedString(
+                "Decrypting the session key failed",
+                comment: "Invalid session key"
             )
         }
     }
@@ -239,11 +245,8 @@ extension CustomError: LocalizedError {
     public var defaultChannel: String = ""
     public var appId: String = ""
     public var deviceID = UIDevice.current.identifierForVendor?.uuidString ?? ""
-    public var privateKey: String = ""
-    public var key: String = "";
+    public var publicKey: String? = ""
     public var hasOldPrivateKeyPropertyInConfig: Bool = false;
-    public var hasDecryptStrategyInConfig: Bool = false;
-    public var decryptStrategyType: String = "private";
 
     public var notifyDownload: (String, Int) -> Void = { _, _  in }
 
@@ -369,73 +372,42 @@ extension CustomError: LocalizedError {
     }
 
     private func decryptFile(filePath: URL, sessionKey: String, version: String) throws {
-        if self.key.isEmpty || sessionKey.isEmpty  || sessionKey.components(separatedBy: ":").count != 2 {
-            print("\(self.TAG) Cannot find decryption key or sessionKey")
+        if self.publicKey != nil && self.publicKey!.isEmpty || sessionKey.isEmpty  || sessionKey.components(separatedBy: ":").count != 2 {
+            print("\(self.TAG) Cannot find public key or sessionKey")
             return
         }
 
-        if (self.decryptStrategyType.equals("private") && !self.key.startsWith("-----BEGIN RSA PRIVATE KEY-----")) {
-            print("\(self.TAG) The decryption strategy is: 'private' and the decryption key provided is not a private key");
-            return;
-        } 
-        if (self.decryptStrategyType.equals("public") && !self.key.startsWith("-----BEGIN RSA PUBLIC KEY-----")) {
-            print("\(self.TAG) The decryption strategy is: 'public' and the key is not a public key");
-            return;
-        }
-
         do {
-            let aesPrivateKey: Data = nil;
+            guard let rsaPublicKey: RSAPublicKey = .load(rsaPublicKey: self.publicKey!) else {
+                print("cannot decode publicKey", self.publicKey!)
+                throw CustomError.cannotDecode
+            }
+
             let sessionKeyArray: [String] = sessionKey.components(separatedBy: ":")
             guard let ivData: Data = Data(base64Encoded: sessionKeyArray[0]) else {
                 print("cannot decode sessionKey", sessionKey)
                 throw CustomError.cannotDecode
             }
 
+//            guard let base64EncodedData = sessionKeyArray[1].data(using: .utf8)! else {
+//                throw NSError(domain: "Invalid session key data", code: 1, userInfo: nil)
+//            }
+            
             guard let sessionKeyDataEncrypted = Data(base64Encoded: sessionKeyArray[1]) else {
                 throw NSError(domain: "Invalid session key data", code: 1, userInfo: nil)
             }
 
-            if (self.decryptStrategyType.equals("private") {
-                guard let rsaPrivateKey: RSAPrivateKey = .load(rsaPrivateKey: self.key) else {
-                    print("cannot decode privateKey", self.key)
-                    throw CustomError.cannotDecode
-                }
-
-                // let sessionKeyArray: [String] = sessionKey.components(separatedBy: ":")
-                // guard let ivData: Data = Data(base64Encoded: sessionKeyArray[0]) else {
-                //     print("cannot decode sessionKey", sessionKey)
-                //     throw CustomError.cannotDecode
-                // }
-
-                // guard let sessionKeyDataEncrypted = Data(base64Encoded: sessionKeyArray[1]) else {
-                //     throw NSError(domain: "Invalid session key data", code: 1, userInfo: nil)
-                // }
-
-                guard let sessionKeyDataDecrypted = try? rsaPrivateKey.decrypt(data: sessionKeyDataEncrypted) else {
-                    throw NSError(domain: "Failed to decrypt session key data", code: 2, userInfo: nil)
-                }
-
-                aesPrivateKey = AES128Key(iv: ivData, aes128Key: sessionKeyDataDecrypted)
+            guard let sessionKeyDataDecrypted = try? rsaPublicKey.decrypt(data: sessionKeyDataEncrypted) else {
+                throw NSError(domain: "Failed to decrypt session key data", code: 2, userInfo: nil)
             }
 
-            if (self.decryptStrategyType.equals("public") {
-                guard let rsaPublicKey: RSAPublicKey = .load(rsaPublicKey: self.key) else {
-                    print("cannot decode public key", self.key)
-                    throw CustomError.cannotDecode
-                }
+            let aesPublicKey = AES128Key(iv: ivData, aes128Key: sessionKeyDataDecrypted)
 
-                guard let sessionKeyDataDecrypted = try? rsaPublicKey.decrypt(data: sessionKeyDataEncrypted) else {
-                    throw NSError(domain: "Failed to decrypt session key data", code: 2, userInfo: nil)
-                }
-
-                aesPrivateKey = AES128Key(iv: ivData, aes128Key: sessionKeyDataDecrypted)
-            }
-            
             guard let encryptedData = try? Data(contentsOf: filePath) else {
                 throw NSError(domain: "Failed to read encrypted data", code: 3, userInfo: nil)
             }
 
-            guard let decryptedData = try? aesPrivateKey.decrypt(data: encryptedData) else {
+            guard let decryptedData = try? aesPublicKey.decrypt(data: encryptedData) else {
                 throw NSError(domain: "Failed to decrypt data", code: 4, userInfo: nil)
             }
 
@@ -553,22 +525,9 @@ extension CustomError: LocalizedError {
                 case .success:
                     self.notifyDownload(id, 71)
                     do {
-                        if (self.privateKey != null && !self.privateKey.isEmpty()) {
-                            self.key = self.privateKey;
-                            self.decryptStrategyType = "private";
-                        }
-
-                        if (self.decryptStrategy != null && !self.hasOldPrivateKeyPropertyInConfig)
+                        if (self.hasOldPrivateKeyPropertyInConfig)
                         {
-                            if (!self.hasOldPrivateKeyPropertyInConfig) {
-                            self.decryptStrategyType = self.decryptStrategy.getType();
-                            self.key = self.decryptStrategy.getKey();
-                            }
-                        }
-
-                        if (!self.hasOldPrivateKeyPropertyInConfig && !self.hasDecryptStrategyInConfig)
-                        {
-                            print("\(self.TAG) There are no decryption keys in the config so the default private key provided by CapGo may be used to try to decrypt which may result in a 'decryptFile fail' error")
+                            print("\(self.TAG) There is still an privateKey property in the config")
                         }
 
                         try self.decryptFile(filePath: fileURL, sessionKey: sessionKey, version: version)
